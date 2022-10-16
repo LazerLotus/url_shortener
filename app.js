bcaa991b6ef298e74fced363b7f8d0f12bcc4cb1
LazerLotus <luan.tsai1991@gmail.com>
2022-10-16 22:58:02 +0800

    feat: init mongoose

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.engine('hbs', exphbs({ defaultLayut: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -10,7 +21,6 @@ app.set('view engine', 'hbs')
 app.get('/', (req, res) => {
   res.render('home')
 })
-
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
