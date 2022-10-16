baba635fa763fc5cefcc11a614fc7da4657b5218
LazerLotus <luan.tsai1991@gmail.com>
2022-10-16 22:47:39 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+
+app.engine('hbs', exphbs({ defaultLayut: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
