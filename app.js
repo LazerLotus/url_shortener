91ed5e58069a3fc09ee2a127e80897eca4b327cf
LazerLotus <luan.tsai1991@gmail.com>
2022-10-23 00:46:04 +0800

    edit note

--- app.js
+++ app.js
@@ -35,17 +35,18 @@ app.post('/', (req, res) => {
   return Urls.find()
     .lean()
     .then((urls) => {
+      // 輸入相同網址時，回傳先前生成過的縮址。
       //check this url exist in db then show the db data
       if (urls.filter((data) => data.original_url.includes(input_url)).length) {
         output_url = urls.filter((data) => data.original_url.includes(input_url))[0].short_url
         return res.render('result', { short_url: output_url, host: host })
       } else {
-        //Generate short url and check whether it is duplicated or not.
+        //Generate short url and check whether it is duplicated or not. 
         do {
           output_url = url_shortener()
         } while (urls.filter((data) => data.short_url.includes(output_url)).length)
 
-        //if it is a new url , generate a new short url
+        //if it is a new url , generate a new short url 如果網址沒有重複，生成新的網址
         return Urls.create({ original_url: input_url, short_url: output_url })
           .then(() => {
             res.render('result', { short_url: output_url, host: host })
