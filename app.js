ca3c6962feb43a5f87d00725c707910c68327fd9
LazerLotus <luan.tsai1991@gmail.com>
2022-10-24 00:19:19 +0800

    refactory: refactor mongoose config

--- app.js
+++ app.js
@@ -3,21 +3,9 @@ const app = express()
 const port = process.env.PORT || 3000
 const exphbs = require('express-handlebars')
 const url_shortener = require('./url_shortener')
-
 const routes = require('./routes')
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
-
+require('./config/mongoose')
 app.engine('hbs', exphbs({ defaultLayut: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
